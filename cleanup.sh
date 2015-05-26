a367dc7e6f52df962adec6cb64ac0bfcba9e1013
Wojciech A. Koszek <wojciech@koszek.com>
2015-05-25 22:45:13 -0700

    Polish comments.

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-find . -type f -name "*.html" -print0 | xargs -0 perl -pi -e 's/ +$//'
-find . -type f -name "*.css" -print0 | xargs -0 perl -pi -e 's/ +$//'
-find . -type f -name "*.erb" -print0 | xargs -0 perl -pi -e 's/ +$//'
-find . -type f -name "*.md" -print0 | xargs -0 perl -pi -e 's/ +$//'
+find . -type f		\
+	-name "*.html"	\
+	-or		\
+	-name "*.css"	\
+	-or		\
+	-name "*.erb"	\
+	-or		\
+	-name "*.md"	\
+	-print0 | xargs -0 perl -pi -e 's/ +$//'
