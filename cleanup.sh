8a776428b4a78fd3e4111b340e09d2fcc2861cae
Wojciech A. Koszek <wojciech@koszek.com>
2015-05-30 21:12:33 -0700

    Don't modify oldblog/ stuff or CSS files.

--- cleanup.sh
+++ cleanup.sh
@@ -4,14 +4,12 @@ rm -rf _.tmp
 find . -type f		\
 	\( -name "*.html"	\
 	-or		\
-	-name "*.css"	\
-	-or		\
 	-name "*.erb"	\
 	-or		\
 	-name "*.md"	\
 	-or		\
 	-name "*.rb" \)	\
-	-print0 > _.tmp
+	-print0 | grep -v oldblog | > _.tmp
 
 cat _.tmp | xargs -n 1 -0 perl -0 -pi -w -e 's/ +$//'
 cat _.tmp | xargs -n 1 -0 perl -0 -pi -w -e "s/\n\n\n/\n/g"
