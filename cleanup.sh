d2b1fa92f37f324b9eaec36bb8c8fe4cf61cf251
Wojciech A. Koszek <wojciech@koszek.com>
2015-05-30 21:11:13 -0700

    Fix cleanup script.

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+rm -rf _.tmp
 find . -type f		\
-	-name "*.html"	\
+	\( -name "*.html"	\
 	-or		\
 	-name "*.css"	\
 	-or		\
 	-name "*.erb"	\
 	-or		\
 	-name "*.md"	\
-	-print0 | xargs -0 perl -pi -e 's/ +$//'
-	perl -0 -pi -w -e "s/\n\n\n/\n/g" *.md
+	-or		\
+	-name "*.rb" \)	\
+	-print0 > _.tmp
+
+cat _.tmp | xargs -n 1 -0 perl -0 -pi -w -e 's/ +$//'
+cat _.tmp | xargs -n 1 -0 perl -0 -pi -w -e "s/\n\n\n/\n/g"
