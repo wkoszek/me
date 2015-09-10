413ccedda75bc33bfa2e2117a8a221fe80924fc9
Wojciech A. Koszek <wojciech@koszek.com>
2015-09-09 23:10:56 -0700

    Add imageoptim.

--- config.rb
+++ config.rb
@@ -32,6 +32,34 @@ end
 dbg("------------ site building started -----------")
 
 # Time.zone = "UTC"
+activate :imageoptim do |options|
+  # Use a build manifest to prevent re-compressing images between builds
+  options.manifest = true
+
+  # Silence problematic image_optim workers
+  options.skip_missing_workers = true
+
+  # Cause image_optim to be in shouty-mode
+  options.verbose = false
+
+  # Setting these to true or nil will let options determine them (recommended)
+  options.nice = true
+  options.threads = true
+
+  # Image extensions to attempt to compress
+  options.image_extensions = %w(.png .jpg .gif .svg)
+
+  # Compressor worker options, individual optimisers can be disabled by passing
+  # false instead of a hash
+  options.advpng    = { :level => 4 }
+  options.gifsicle  = { :interlace => false }
+  options.jpegoptim = { :strip => ['all'], :max_quality => 100 }
+  options.jpegtran  = { :copy_chunks => false, :progressive => true, :jpegrescan => true }
+  options.optipng   = { :level => 6, :interlace => false }
+  options.pngcrush  = { :chunks => ['alla'], :fix => false, :brute => false }
+  options.pngout    = { :copy_chunks => false, :strategy => 0 }
+  options.svgo      = {}
+end
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
