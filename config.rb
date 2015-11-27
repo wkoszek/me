34ae1b20d641f94ce5f73e1dc09007cec2bf5e48
Wojciech A. Koszek <wojciech@koszek.com>
2015-11-27 13:19:52 -0800

    Add deployment configuration.

--- config.rb
+++ config.rb
@@ -230,3 +230,8 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
+# To deploy to a remote branch via git (e.g. gh-pages on github):
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = "gh-pages"
+end
