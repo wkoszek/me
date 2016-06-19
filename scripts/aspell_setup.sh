323221a79171274e9f52d82471ba625320d3d5a2
Wojciech Adam Koszek <wojciech@koszek.com>
2016-06-19 01:08:08 -0700

    Bring changes to aspell config.

--- scripts/aspell_setup.sh
+++ scripts/aspell_setup.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-F=aspell6-en-2015.04.24-0.tar.bz2
+set -x
+set -e
+
+BN=aspell6-en-2015.04.24-0
+F=${BN}.tar.bz2
 URL=ftp://ftp.gnu.org/gnu/aspell/dict/en/${F}
 
 echo "# setting up Aspell dictionaries!!"
 (
 	cd data
-	curl $URL
+	curl -o $F $URL
 	tar xjf $F
 	rm $F
-	ln -s aspell* aspell-dict
+	ln -s ${BN} aspell-dict
 	cd aspell-dict
 	./configure
 	make
