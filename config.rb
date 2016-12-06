7cab7d2cb2db92c353553c6f2de224b730564874
Wojciech Adam Koszek <wojciech@koszek.com>
2016-12-06 00:01:32 -0800

    Add the name to the main blog.

--- config.rb
+++ config.rb
@@ -48,6 +48,7 @@ end
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "blog"
+  blog.name = "wkoszek"
 
   blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
