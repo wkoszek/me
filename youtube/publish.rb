8487e30983e65fca3d83523320c9b15a48fbc066
Wojciech Adam Koszek <wojciech@koszek.com>
2017-04-08 15:58:14 -0700

    Fix code some more.

--- youtube/publish.rb
+++ youtube/publish.rb
@@ -2,9 +2,9 @@
 
 require 'yaml'
 require 'pp'
+require_relative 'upload_video.rb'
 
-d = YAML.load(File.read("desc.yml"))
-
+d = YAML.load(File.read("wkoszek-youtube.yml"))
 print YAML.dump(d)
 
 #File.write("wojtek", YAML.dump(d))
--- youtube/upload_video.rb
+++ youtube/upload_video.rb
@@ -89,4 +89,6 @@ def main
   end
 end
 
-main
+if __FILE__ == $0
+  main
+end
